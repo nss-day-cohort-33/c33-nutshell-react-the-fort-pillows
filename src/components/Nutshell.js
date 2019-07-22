6641fe5109df1f8be319e11dba2d13bd51fdbcb9
Shane Miller <shanemiller89@gmail.com>
2019-07-22 15:31:17 -0500

    file structure set up, db set up, login screen set up

--- src/components/Nutshell.js
+++ src/components/Nutshell.js
@@ -1,7 +1,9 @@
 import React, { Component } from "react";
+import LoginForm from "./Login/Login"
 import NavBar from "./nav/NavBar";
 import ApplicationViews from "./ApplicationViews";
 import "./Nutshell.css";
+import 'semantic-ui-css/semantic.min.css'
 
 class Nutshell extends Component {
   render() {

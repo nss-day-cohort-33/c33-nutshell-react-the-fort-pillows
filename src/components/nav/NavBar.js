623763c97ff11765988d462b5f8f14ff835480f0
Shane Miller <shanemiller89@gmail.com>
2019-07-22 21:53:54 -0500

    semantic navbar

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -1,41 +1,100 @@
 import React, { Component } from "react";
+import { Button, Dropdown, Menu, Icon } from "semantic-ui-react";
 import { Link } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
 
-class NavBar extends Component {
+export default class NavBar extends Component {
+  state = { activeItem: "home" };
+
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name });
+
   render() {
+    const { activeItem } = this.state;
+
     return (
-      <nav className="navbar navbar-light light-blue flex-md-nowrap p-0 shadow">
-        <ul className="nav nav-pills nav-fill">
-          <li className="nav-item">
-            <Link className="nav-link" to="/">
-              Dashboard
+      <Menu size="large">
+        <Menu.Item header as='h2'>
+          Nutshell
+        </Menu.Item>
+        <Dropdown item icon="sidebar" size=''>
+          <Dropdown.Menu>
+            <Link to="/">
+              <Dropdown.Item>Dashboard</Dropdown.Item>
             </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/news">
-              News
+            <Link to="/news">
+            <Dropdown.Item>News</Dropdown.Item>
             </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/events">
-              Events
+            <Link to="/events">
+            <Dropdown.Item>Events</Dropdown.Item>
             </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/tasks">
-              Tasks
+            <Link to="/tasks">
+            <Dropdown.Item>Tasks</Dropdown.Item>
             </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/friends">
-              Friends
+            <Link to="/friends">
+            <Dropdown.Item>Friends</Dropdown.Item>
             </Link>
-          </li>
-        </ul>
-      </nav>
+          </Dropdown.Menu>
+        </Dropdown>
+
+        <Menu.Menu position="right">
+          <Menu.Item>
+          <Menu.Item header as='h3'>
+          <Icon name='user' />
+            Username
+          </Menu.Item>
+            <Button primary>Logout</Button>
+          </Menu.Item>
+        </Menu.Menu>
+      </Menu>
     );
   }
 }
 
-export default NavBar;
+{
+  /* <Menu.Item
+  name='messages'
+  active={activeItem === 'messages'}
+  onClick={this.handleItemClick}
+/> */
+}
+
+// import React, { Component } from "react";
+// import { Link } from "react-router-dom";
+// import "bootstrap/dist/css/bootstrap.min.css";
+
+// class NavBar extends Component {
+//   render() {
+//     return (
+//       <nav className="navbar navbar-light light-blue flex-md-nowrap p-0 shadow">
+//         <ul className="nav nav-pills nav-fill">
+//           <li className="nav-item">
+//             <Link className="nav-link" to="/">
+//               Dashboard
+//             </Link>
+//           </li>
+//           <li className="nav-item">
+//             <Link className="nav-link" to="/news">
+//               News
+//             </Link>
+//           </li>
+//           <li className="nav-item">
+//             <Link className="nav-link" to="/events">
+//               Events
+//             </Link>
+//           </li>
+//           <li className="nav-item">
+//             <Link className="nav-link" to="/tasks">
+//               Tasks
+//             </Link>
+//           </li>
+//           <li className="nav-item">
+//             <Link className="nav-link" to="/friends">
+//               Friends
+//             </Link>
+//           </li>
+//         </ul>
+//       </nav>
+//     );
+//   }
+// }
+
+// export default NavBar;
